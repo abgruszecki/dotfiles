82de155d572b461e917d0873fa1960ab4db8294a
Aleksander Boruch-Gruszecki <research@abgru.me>
2025-06-14 15:42:47 -0500

    .

--- roaming-link-configs.sh
+++ roaming-link-configs.sh
@@ -5,5 +5,5 @@ test -e dotfiles || {
     echo >&2 'Missing dir, exiting: ~/dotfiles/'
     exit 1
 }
-ln -s dotfiles/tmux/.config/tmux/tmux.conf .tmux.conf
-ln -s dotfiles/bash/.bash.d .bash.d
+ln -sf dotfiles/tmux/.config/tmux/tmux.conf .tmux.conf
+ln -sf dotfiles/bash/.bash.d .bash.d
