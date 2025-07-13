cbd23c3aab3a518c7f93202741b371009b2297ed
Aleksander Boruch-Gruszecki <research@abgru.me>
2025-07-13 14:25:53 -0500

    robolang

--- bash/.bash.d/link-local.sh
+++ bash/.bash.d/link-local.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+log() {
+    echo >&2 "$@"
+}
+
 aloud() {
     { printf '$' && printf ' %q' "$@" && printf '\n'; } >&2
     "$@"
@@ -7,5 +11,11 @@ aloud() {
 
 
 machine_name=$1
+
+test ! -z "$machine_name" || {
+    log Usage: "$0" "<machine-name>"
+    exit 1
+}
+
 
 aloud ln -sf "$HOME/dotfiles-private/$1/bash-local" "$HOME/.bash.d/local"
--- bash/.bash.d/source-all.sh
+++ bash/.bash.d/source-all.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 prefix=~/.bash.d
 . $prefix/local/source-all.sh
